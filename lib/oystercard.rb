ab4c61628d48255d7c689cc35c82859332784ba1
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-10 16:45:45 +0100

    Add touch in and out methods. Add minimum required balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,42 @@
 class Oystercard
     BALANCE = 0
-    attr_reader :balance
+    MAX_BALANCE = 90
+    FARE = 1
+    attr_reader :balance, :status
+    #attr_accessor :max_balance
+
 
   def initialize(balance = BALANCE)
     @balance = balance
+    @max_balance = MAX_BALANCE
+    @status = false
   end
 
   def top_up(top_up_value)
+    fail "Card has reached its £#{@max_balance} limit!" if top_up_value + balance > MAX_BALANCE
     @balance += top_up_value
   end
+
+  def deduct
+    @balance -= FARE
+  end
+
+  def in_journey?
+    @status
+  end
+
+ #  def card_full?
+ #   @balance > @max_balance
+ # end
+
+  def touch_in
+    fail "Not enough money on card! Your balance is £#{@balance}" if balance < FARE
+    @status = true
+    deduct
+  end
+
+  def touch_out
+    @status = false
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,9 +3,56 @@ require 'oystercard'
 describe Oystercard do
   it { is_expected.to have_attributes(:balance => 0) }
 
+  it { should respond_to(:top_up).with(1).argument  }
   it 'tops up oystercard' do
     oystercard = Oystercard.new
-    oystercard.top_up(6)
-    expect(oystercard.balance).to eq(6)
+    oystercard.top_up(2)
+    expect(oystercard.balance).to eq(2)
   end
+
+  it "Raises an error when card is full" do
+    max_balance = Oystercard::MAX_BALANCE
+    oystercard = Oystercard.new
+    oystercard.top_up(max_balance)
+    expect { oystercard.top_up 1 }.to raise_error("Card has reached its £#{max_balance} limit!")
+  end
+
+  it "Deducts the fare from the balance" do
+    fare = Oystercard::FARE
+    oystercard = Oystercard.new
+    oystercard.top_up(10)
+    expect(oystercard.deduct).to eq(10 - fare)
+  end
+
+  it "Should confirm passenger is in journey" do
+    oystercard = Oystercard.new
+    oystercard.top_up(10)
+    oystercard.touch_in
+    expect(oystercard.status).to be true
+  end
+
+  it "Should touch in at journey beginning" do
+    fare = Oystercard::FARE
+    oystercard = Oystercard.new
+    oystercard.top_up(10)
+    oystercard.touch_in
+    expect(oystercard.status).to be true
+    expect(oystercard.balance).to eq(10 - fare)
+  end
+
+  it "Should touch out at journey end" do
+    oystercard = Oystercard.new
+    oystercard.top_up(10)
+    oystercard.touch_in
+    oystercard.touch_out
+    expect(oystercard.status).to be false
+  end
+
+  it 'should prevent travelling with balance less than £1' do
+    fare = Oystercard::FARE
+    balance = Oystercard::BALANCE
+    oystercard = Oystercard.new
+    expect { oystercard.touch_in }.to raise_error("Not enough money on card! Your balance is £#{balance}")
+  end
+
 end
